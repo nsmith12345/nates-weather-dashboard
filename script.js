65cc84af553e53c652307f8f098b357fcd9f88ba
nsmith12345 <nsmith_2003@hotmail.com>
2022-03-30 23:05:42 -0400

    added variables

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+//***********************Variables*******************************/
+var citiesList = document.querySelector("#citiesList"); 
+var currentDate = document.querySelector(#)
+var nameOfCity = document.querySelector
+var temperature = document.querySelector
+var highTemperature = document.querySelector
+var lowTemperature = document.querySelector
+var humidity = document.querySelector
+var uvIndex = document.querySelector 
+var todayWeather = document.querySelector 
+var currentHour = moment().hour(); //Stores the current hour for comparison of each row.
 
+getweather
